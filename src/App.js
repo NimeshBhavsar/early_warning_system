81018112782e03baf7dda85da6bec1600a64fb3c
Nimesh <nimesh.j@mu-sigma.com>
2024-10-07 16:34:53 +0530

    Header, Navbar set

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import mslogo from './resources/MS_Logo.png';
+import thdlogo from './resources/THD_Logo.png';
 import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import HomePage from "./pages/Homepage";
+import AnalysisPage from "./pages/Analysispage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div className="app-container">
+          {/* Header */}
+          <header className="header">
+            <img id='thdlogo' src={thdlogo} alt="THD Logo" />
+
+
+            <h1>Early Warning System</h1>
+            <img id='mslogo' src={mslogo} alt="MS Logo" />
+
+          </header>
+
+          {/* Sidebar and content */}
+          <div className="main-container">
+            {/* Sidebar */}
+            <nav className="sidebar">
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/analysis">Analysis</Link>
+                </li>
+              </ul>
+            </nav>
+
+            {/* Content area */}
+            <div className="content">
+              <Routes>
+                <Route exact path="/" component={HomePage} />
+                <Route path="/analysis" component={AnalysisPage} />
+              </Routes>
+            </div>
+          </div>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Analysispage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AnalysisPage() {
+    return (
+        <div>
+            <h2>Analysis Page</h2>
+            <p>This is the Analysis Page!</p>
+        </div>
+    );
+}
+
+export default AnalysisPage;
